ae3d3e9dfcd28fe3921f3f40e49cf4faf47da867
M-LOGESH <logeshm724@gmail.com>
2025-10-24 23:20:32 +0530

    fix: file path changed

--- FarmIQ/vite.config.js
+++ FarmIQ/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        analysis: 'src/pages/analysis.html',
+        irrigation: 'src/pages/irrigation.html',
+        livecrop: 'src/pages/livecrop.html'
+      }
+    }
+  }
 })
